c5bd1ef6c518aa946def00029c43b0d52b5a0053
Gillian Trethewey <gilliantrethewey100@gmail.com>
2024-04-10 19:32:47 -0700

    tests GET route for front and backends

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const handleClick = async () => {
+    const response = await axios.get("http://localhost:3025");
+    console.log("RESPONSE", response);
+  };
   return (
     <ChakraProvider>
       <Box>HI FROM APP</Box>
-      <Button colorScheme="purple">BUTTON</Button>
+      <Button colorScheme="purple" onClick={handleClick}>
+        BUTTON
+      </Button>
     </ChakraProvider>
   );
 }
